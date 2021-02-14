c573fb52752443896010b3ff6b140042a1868ded
devizer <devizer@users.github.io>
2021-02-14 13:43:06 +0200

    azure-pipelines.yml, 8th

--- 0-Rebuild-All.cmd
+++ 0-Rebuild-All.cmd
@@ -1,12 +1,16 @@
 If Not Defined VER Set VER=0.1.2.3
 for %%d in (Universe.SqlInsights.AspNetLegacy Universe.SqlInsights.NetCore Universe.SqlInsights.Shared Universe.SqlInsights.SqlServerStorage) DO (
+  echo Clean src\%%d\bin
   rd /q /s src\%%d\bin
+  echo Clean src\%%d\obj
   rd /q /s src\%%d\obj
 )
 
 pushd src\
+Echo Nuget Restore
 nuget restore -verbosity quiet
 for %%c in (Release Debug) Do (
+  echo MSBuild %%c
   msbuild /t:Restore,Rebuild /v:m /p:Configuration=%%c /p:Version=%VER% /p:VersionSuffix=beta Universe.SqlInsights.sln
 )
 rem dotnet build /v:m /p:Version=%VER% Universe.SqlInsights.sln
