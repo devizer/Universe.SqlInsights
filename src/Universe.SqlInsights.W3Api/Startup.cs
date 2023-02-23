118bef9b5aff171ff978d98d93a49d046dd43a18
devizer <devizer@users.github.io>
2023-02-23 20:04:21 +0200

    TODO: START MIGRATE ON STARTUP, crash if fail ***NO_CI***

--- src/Universe.SqlInsights.W3Api/Startup.cs
+++ src/Universe.SqlInsights.W3Api/Startup.cs
@@ -83,6 +83,7 @@ namespace Universe.SqlInsights.W3Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            // TODO: START MIGRATE ON STARTUP, crash if fail
             PreJit(logger);
             app.ValidateSqlInsightsServices();
             app.UseSqlInsights();
