205a2af9a70f90084b31dbcea315b841eb4c7fb8
devizer <devizer@users.github.io>
2023-02-25 06:27:00 +0200

    9th Try Ram Disk. Disable firewall? Fix w3api: fix caching of strings, Fix w3app: Double Action Summary Fetch & Render, v2

--- scripts/Create-Windows-Ram-Disk.ps1
+++ scripts/Create-Windows-Ram-Disk.ps1
@@ -9,6 +9,21 @@ function Say { param( [string] $message )
     Write-Host "$message" -ForegroundColor Yellow
 }
 
+# List of IP Addresses
+Get-NetIPAddress | ft
+$ip=""
+
+Get-NetIPAddress | % {$_.IpAddress} | where { $_.StartsWith("10.") } | % { if ($_) {$ip=$_} }
+Get-NetIPAddress | % {$_.IpAddress} | where { $_.StartsWith("172.") } | % { if ($_) {$ip=$_} }
+Get-NetIPAddress | % {$_.IpAddress} | where { $_.StartsWith("192.") } | % { if ($_) {$ip=$_} }
+Say "DETECTED IP: [$ip]"
+
+$size=580;
+Say "RAM DISK SIZE: $size MB";
+Say "RAM DISK DRIVE: '$($ENV:RAM_DISK)'";
+
+
+
 $M=Get-Module -ListAvailable ServerManager; Import-Module -ModuleInfo $M;
 
 # Say "WINDOWS Features"; Get-WindowsFeature *; echo "";
@@ -23,19 +38,6 @@ $M=Get-Module -ListAvailable ServerManager; Import-Module -ModuleInfo $M;
 
 
 # https://github.com/aso930/CreateRAMDISK
-
-# List of IP Addresses
-Get-NetIPAddress | ft
-$ip=""
-
-Get-NetIPAddress | % {$_.IpAddress} | where { $_.StartsWith("172.") } | % { if ($_) {$ip=$_} }
-Get-NetIPAddress | % {$_.IpAddress} | where { $_.StartsWith("10.") } | % { if ($_) {$ip=$_} }
-Get-NetIPAddress | % {$_.IpAddress} | where { $_.StartsWith("192.") } | % { if ($_) {$ip=$_} }
-Say "DETECTED IP: [$ip]"
-
-$size=580;
-Say "RAM DISK SIZE: $size MB";
-Say "RAM DISK DRIVE: '$($ENV:RAM_DISK)'";
 
 
 
