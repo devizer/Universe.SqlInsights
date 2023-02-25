0d887d766af8e147e7beb39b4667909393b0db97
devizer <devizer@users.github.io>
2023-02-25 10:09:40 +0200

    18? 10.* is priority Try Ram Disk. Disable firewall? Fix w3api: fix caching of strings, Fix w3app: Double Action Summary Fetch & Render, v2

--- scripts/Create-Windows-Ram-Disk.ps1
+++ scripts/Create-Windows-Ram-Disk.ps1
@@ -36,6 +36,8 @@ Get-NetIPAddress | % {$_.IpAddress} | where { $_.StartsWith("10.") } | % { if ($
 Get-NetIPAddress | % {$_.IpAddress} | where { $_.StartsWith("192.") } | % { if ($_) {$ip=$_} }
 Say "DETECTED IP: [$ip]"
 
+if ()
+
 $size=3000;
 Say "RAM DISK SIZE: $size MB";
 Say "RAM DISK DRIVE: '$($ENV:RAM_DISK)'";
@@ -56,16 +58,26 @@ $M=Get-Module -ListAvailable ServerManager; Import-Module -ModuleInfo $M;
 
 
 cd ~
+Say "New-iscsivirtualdisk ..."
 New-iscsivirtualdisk -path ramdisk:RAMDISK1.vhdx -size ([int]$size * 1MB)
+Say "New-IscsiServerTarget ..."
 New-IscsiServerTarget Target1 -InitiatorId IPAddress:$ip
+Say "Add-IscsiVirtualDiskTargetMapping ..."
 Add-IscsiVirtualDiskTargetMapping -TargetName Target1 -Path ramdisk:RAMDISK1.vhdx -Lun 1
+Say "Start-Service msiscsi ..."
 Start-Service msiscsi
+Say "New-IscsiTargetPortal ..."
 New-IscsiTargetPortal -TargetPortalAddress $ip
+Say "Connect-IscsiTarget ..."
 Get-IscsiTarget | Connect-IscsiTarget
+Say "Set-Disk -IsOffline False ..."
 Get-IscsiConnection | Get-Disk | Set-Disk -IsOffline $False
+Say "Initialize-Disk ..."
 Get-IscsiConnection | Get-Disk | Initialize-Disk -PartitionStyle MBR
 # -AssignDriveLetter  random?
+Say "New-Partition and Format-Volume ..."
 Get-IscsiConnection | Get-Disk | New-Partition -UseMaximumSize -DriveLetter ([char]"$($ENV:RAM_DISK)") | Format-Volume -FileSystem NTFS -NewFileSystemLabel RamDisk -Full -Force
+Say "COMPLEEEEETE"
 
 echo ""
 Say "GET-DISK"
@@ -75,3 +87,13 @@ Get-PSDrive | ft
 
 $ram=Get-Ram;
 Say "Total RAM: $($ram.Total.ToString("n0")) MB. Free: $($ram.Free.ToString("n0")) MB ($([Math]::Round($ram.Free * 100 / $ram.Total, 1))%)"
+
+if (Test-Path -Path "$($ENV:RAM_DISK):\") {
+    Say "RAM Drive "$($ENV:RAM_DISK):\" exists"
+    $dataDir="$($ENV:RAM_DISK):\DB";
+    New-Item $dataDir -type directory -force -EA SilentlyContinue | out-null
+    echo "##vso[task.setvariable variable=DB_DATA_DIR]$dataDir"
+    Say "DB_DATA_DIR env is set to $dataDir";
+} else {
+    Say "RAM Drive "$($ENV:RAM_DISK):\" not found"
+}
