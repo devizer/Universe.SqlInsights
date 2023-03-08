7560a6c55c93cf08f3bb03dea020a9d8967f0495
devizer <devizer@users.github.io>
2023-03-09 01:41:57 +0200

    6 Preliminary w3app docker image

--- scripts/Linux/Build-W3App-Linux-Docker-Image.sh
+++ scripts/Linux/Build-W3App-Linux-Docker-Image.sh
@@ -14,7 +14,7 @@ docker buildx inspect --bootstrap
 Say "Supported architectures"
 docker buildx ls
 
-# BUILD W3APP APP
+Say "BUILD W3APP APP"
 script=https://raw.githubusercontent.com/devizer/test-and-build/master/install-build-tools-bundle.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash >/dev/null
 export NODE_VER=v14.19.1 SKIP_NPM_UPGRADE=True
 time (script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node)
@@ -25,6 +25,9 @@ time yarn build
 cp -f -a build "$dir"
 popd
 
+Say "BUILD X64 ONLY CONTAINER"
+time docker build -t w3app-x64 .
+
 docker_version="$(date +%F)"
 Say "Docker Image version: [${docker_version}]"
 # docker image rm -f $(docker image ls -aq)
@@ -33,10 +36,7 @@ export TAGS="-t devizervlad/sqlinsights-dashboard:v${docker_version} -t devizerv
 export BASE_IMAGE='nginx:latest'
 platform="linux/amd64,linux/arm32v5,linux/arm32v6,linux/arm32v7,linux/arm64v8,linux/i386,linux/mips64le,linux/ppc64le,linux/s390x"
 
-Say "Try x64 only"
-time docker build -t w3app-x64 .
-
-Say "Build all the platforms"
+Say "BUILD ALL THE PLATFORMS"
 # revert to --push
 time docker buildx build \
   --platform $platform --load \
