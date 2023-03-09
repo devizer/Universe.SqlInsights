43dd4a6f5d5b4542e33cc993a95920cb5e880446
devizer <devizer@users.github.io>
2023-03-09 14:33:36 +0200

    REL! Bash Shell, Fix Quotes & Version for api docker images, renaming images, small set of rids, adding w3app.tar.gz

--- scripts/Build-Github-Release-Artifacts.sh
+++ scripts/Build-Github-Release-Artifacts.sh
@@ -6,25 +6,33 @@ function Filter-7z() {
   grep "archive\|bytes" || true;
 }
 
+git clone "$BUILD_REPOSITORY_URI" /tmp/src-copy
+pushd /tmp/src-copy
+revision="$(set TZ=GMT; git log -n 999999 --date=raw --pretty=format:"%cd" | wc -l)"
+popd
+version="2.1.${revision}"
+export SQLINSIGHTS_VERSION="v${version}"
+
 pushd src/Universe.SqlInsights.W3Api
 public=$(pwd)/bin/public
+echo "$SQLINSIGHTS_VERSION" > "$public"/VERSION.txt
 
-Say "Grap universe.sqlinsights.w3app"
+Say "Grab universe.sqlinsights.w3app"
 pushd $BUILD_REPOSITORY_LOCALPATH/src/universe.sqlinsights.w3app/build
 pwd
-7z a -mx=9 -ms=on -mqs=on "$public"/w3app.7z . | Filter-7z
+7z a -mx=9 "$public"/w3app.zip . | Filter-7z
 time tar cf - . | pigz -p $(nproc) -b 128 -9  > "$public"/w3app.tar.gz
 popd
 
-prefix="sqlinsights-w3api"
+prefix="sqlinsights-dashbaord"
 mkdir -p "$public"
 n=0
 rids="osx-x64 osx-arm64 win-x64 win-x86 win-arm64 win-arm linux-x64 linux-arm linux-arm64 linux-musl-x64 osx.10.10-x64 osx.10.11-x64"
-rids="linux-x64 linux-arm linux-arm64"
+# rids="linux-x64 linux-arm linux-arm64"
 for r in $rids; do
   n=$((n+1))
   Say "#${n}: BUILD [$r]"
-  dotnet publish --self-contained -r $r -f net6.0 -o bin/plain/$r -v:q
+  dotnet publish --self-contained -r $r -f net6.0 -o bin/plain/$r -v:q -p:Version=$SQLINSIGHTS_VERSION
   pushd bin/plain/$r
     chmod 644 *.dll
     test -s Universe.SqlInsights.W3Api && chmod 755 Universe.SqlInsights.W3Api
@@ -43,13 +51,8 @@ done
 
 cp -r -a "$public" "$SYSTEM_ARTIFACTSDIRECTORY"/
 
-git clone "$BUILD_REPOSITORY_URI" /tmp/src-copy
-pushd /tmp/src-copy
-revision="$(set TZ=GMT; git log -n 999999 --date=raw --pretty=format:"%cd" | wc -l)"
-popd
-version="0.0.${revision}"
-export SQLINSIGHTS_VERSION="v${version}-pre"
 Say "Create Github Release ${version}"
-gh release create -t "SqlInsights W3 API" -n "v${version}" -p "$SQLINSIGHTS_VERSION" "$public"/*
+# -p mean pre-release
+gh release create -t "SqlInsights Dashboard Web API" -n "v${version}" "$SQLINSIGHTS_VERSION" "$public"/*
 
 popd
