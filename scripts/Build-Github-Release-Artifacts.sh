dc48ef28e21c9c3a809d2ffb648543ab4849f5b0
devizer <devizer@users.github.io>
2023-03-09 06:38:21 +0200

    5 fix dependencies, adding guthub release, fix exclude, add w3app.7z, fix 7z error, xz parallel

--- scripts/Build-Github-Release-Artifacts.sh
+++ scripts/Build-Github-Release-Artifacts.sh
@@ -13,7 +13,7 @@ for r in osx-x64 win-x64 win-x86 win-arm64 win-arm linux-x64 linux-arm linux-arm
       7z a -tzip -mx=9 "$public"/$prefix-$r.zip *
       7z a -t7z -mx=9 -ms=on -mqs=on "$public"/$prefix-$r.7z *
     else
-      tar cf - . | xz -9 -e -z > "$public"/$prefix-$r.tar.xz
+      tar cf - . | xz -9 -e -z -T0 > "$public"/$prefix-$r.tar.xz
       tar cf - . | gzip -9 -c  > "$public"/$prefix-$r.tar.gz
     fi
   popd
