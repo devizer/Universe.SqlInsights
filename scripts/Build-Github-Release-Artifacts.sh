b3773300b33c661c6e10a08414060d1543035d6a
devizer <devizer@users.github.io>
2023-03-09 06:49:53 +0200

    8 fix dependencies, adding guthub release, fix exclude, add w3app.7z, fix 7z error, xz parallel

--- scripts/Build-Github-Release-Artifacts.sh
+++ scripts/Build-Github-Release-Artifacts.sh
@@ -4,17 +4,17 @@ public=$(pwd)/bin/public
 prefix="sqlinsights-w3api"
 mkdir -p "$public"
 n=0
-for r in osx-x64 win-x64 win-x86 win-arm64 win-arm linux-x64 linux-arm linux-arm64 linux-musl-x64; do
+for r in osx-x64 win-x64 win-x86 win-arm64 win-arm linux-x64 linux-arm linux-arm64 linux-musl-x64 osx.10.10-x64 osx.10.11-x64; do
   n=$((n+1))
   Say "#${n}: BUILD [$r]"
   dotnet publish --self-contained -r $r -f net6.0 -o bin/plain/$r -v:q
   pushd bin/plain/$r
     if [[ "$r" == "win"* ]]; then
-      7z a -tzip -mx=9 "$public"/$prefix-$r.zip *
-      7z a -t7z -mx=9 -ms=on -mqs=on "$public"/$prefix-$r.7z *
+      time 7z a -tzip -mx=9 "$public"/$prefix-$r.zip *
+      time 7z a -t7z -mx=9 -ms=on -mqs=on "$public"/$prefix-$r.7z *
     else
-      tar cf - . | xz -9 -e -z -T0 > "$public"/$prefix-$r.tar.xz
-      tar cf - . | gzip -9 -c  > "$public"/$prefix-$r.tar.gz
+      time tar cf - . | xz -9 -e -z -T0 > "$public"/$prefix-$r.tar.xz
+      time tar cf - . | gzip -9 -c  > "$public"/$prefix-$r.tar.gz
     fi
   popd
 done
