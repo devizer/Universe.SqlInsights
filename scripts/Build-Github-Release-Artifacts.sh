1bc71d65f19c3577b2bfcd8e0046859c7976a467
devizer <devizer@users.github.io>
2023-03-09 07:05:16 +0200

    13 add w3app.7z, fix 7z error, xz2 parallel

--- scripts/Build-Github-Release-Artifacts.sh
+++ scripts/Build-Github-Release-Artifacts.sh
@@ -24,6 +24,7 @@ for r in osx-x64 win-x64 win-x86 win-arm64 win-arm linux-x64 linux-arm linux-arm
       # time tar cf - . | xz -9 -e -z -T0 > "$public"/$prefix-$r.tar.xz
       time tar cf - . | 7za a dummy -txz -mx=1 -si -so > "$public"/$prefix-$r.tar.xz
       time tar cf - . | gzip -9 -c  > "$public"/$prefix-$r.tar.gz
+      # pigz -p 8 -b 128 -9
     fi
   popd
 done
