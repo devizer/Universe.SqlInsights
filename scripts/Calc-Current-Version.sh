9e59439aa28476d7cdbff39d69a0cee668f6458d
devizer <devizer@users.github.io>
2023-03-11 02:39:13 +0200

    docker image fix4, add wwwroot, Split into steps

---
+++ scripts/Calc-Current-Version.sh
@@ -0,0 +1,16 @@
+set -eu; set -o pipefail
+
+git clone "$BUILD_REPOSITORY_URI" /tmp/src-copy
+pushd /tmp/src-copy
+revision="$(set TZ=GMT; git log -n 999999 --date=raw --pretty=format:"%cd" | wc -l)"
+popd
+version="0.0.${revision}"
+export SQLINSIGHTS_VERSION="v${version}"
+export SQLINSIGHTS_VERSION_SHORT="$version"
+
+echo "##vso[task.setvariable variable=SQLINSIGHTS_VERSION]$SQLINSIGHTS_VERSION"
+echo "##vso[task.setvariable variable=SQLINSIGHTS_VERSION_SHORT]$SQLINSIGHTS_VERSION_SHORT"
+
+Say "SQLINSIGHTS_VERSION: [$SQLINSIGHTS_VERSION]"
+Say "SQLINSIGHTS_VERSION_SHORT: [$SQLINSIGHTS_VERSION_SHORT]"
+
