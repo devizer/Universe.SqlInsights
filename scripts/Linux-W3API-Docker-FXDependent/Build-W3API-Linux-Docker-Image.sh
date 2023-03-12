aa0c6e646b22187f301f3c2884682b55dafad981
devizer <devizer@users.github.io>
2023-03-12 02:47:40 +0200

    fxdependent docker image via COPY, DbProviderFactory Configuration (system|microsoft). Microsoft provider for docker images,

--- scripts/Linux-W3API-Docker-FXDependent/Build-W3API-Linux-Docker-Image.sh
+++ scripts/Linux-W3API-Docker-FXDependent/Build-W3API-Linux-Docker-Image.sh
@@ -6,6 +6,8 @@ platform="linux/amd64,linux/arm/v7,linux/arm64"
 
 export TAGS="-t devizervlad/$TARGET_IMAGE:${SQLINSIGHTS_VERSION} -t devizervlad/$TARGET_IMAGE:latest"
 
+cp -r -a $BUILD_REPOSITORY_LOCALPATH/src/Universe.SqlInsights.W3Api/bin/fxdepend/. build/
+
 Say "BUILD FXDependent ALL THE PLATFORMS Docker Images"
 # revert to --push
 time docker buildx build \
