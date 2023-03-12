24c4646b5f0ade9338f4348dce3d17d44caaf017
devizer <devizer@users.github.io>
2023-03-12 10:31:47 +0200

    Fix Interactive Tests on mac/linux DB of Test cases with hostname, Ignore DB Name for SQLINSIGHTS_CONNECTION_STRING tests ***NO_CI***

--- src/run-w3api-and-w3app-on-mac-or-linux.sh
+++ src/run-w3api-and-w3app-on-mac-or-linux.sh
@@ -6,7 +6,7 @@ pkill dotnet; pkill node;
 cd ~; rm -rf ~/source/sqlinsights
 test ! -d ~/source/sqlinsights && git clone https://github.com/devizer/Universe.SqlInsights ~/source/sqlinsights
 
-export ConnectionStrings__SqlInsights="TrustServerCertificate=True;Server=192.168.0.42;Database=SqlInsights_v4;User ID=sa;Password=\`1qazxsw2"
+export ConnectionStrings__SqlInsights="TrustServerCertificate=True;Server=192.168.0.42;Database=SqlInsights vNext for $(hostname);User ID=sa;Password=\`1qazxsw2"
 export SQLINSIGHTS_CONNECTION_STRING="$ConnectionStrings__SqlInsights"
 cd ~/source/sqlinsights/src/Universe.SqlInsights.SqlServerStorage.Tests; git pull; dotnet test -f net5.0 -c Release
 
@@ -20,9 +20,11 @@ if [[ "$(uname -s)" == Darwin ]]; then
   yarn build
   export PORT=6060
   cd build
-  nohup npx serve &
+  # nohup npx serve &
+  dotnet serve --version 2>nul || dotnet tool install --global dotnet-serve
+  dotnet serve --version
+  nohup dotnet serve -p 6060 -o 2>&1 | tee ~/w3app.log &
+
 fi
 
 tail -f ~/w3api.log
-
-
