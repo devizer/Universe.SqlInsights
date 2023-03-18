5b402b41c4c66cfb60402b917d129fb034b0d06b
devizer <devizer@users.github.io>
2023-03-18 20:51:44 +0200

    Added CrossPlatformLogger and log files for Windows Service. Added Experimental InternalVersion RowVersion Not Null

---
+++ src/Universe.SqlInsights.NetCore/NetCoreLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Universe.SqlInsights.Shared;
+
+namespace Universe.SqlInsights.NetCore
+{
+    public class NetCoreLogger : ICrossPlatformLogger
+    {
+        private ILogger Logger;
+
+        public NetCoreLogger(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Log(LogSeverity severity, string message, Exception exception)
+        {
+            if (severity == LogSeverity.Info)
+                Logger?.LogInformation(message);
+#if !NETCOREAPP1_1 && !NETCOREAPP1_0            
+            if (severity == LogSeverity.Warning)
+                Logger?.LogWarning(exception, message);
+            if (severity == LogSeverity.Error)
+                Logger?.LogError(exception, message);
+#else
+            if (severity == LogSeverity.Warning)
+                Logger?.LogWarning(message + (exception == null ? null : $"{Environment.NewLine}{exception}"));
+            if (severity == LogSeverity.Error)
+                Logger?.LogError(message+ (exception == null ? null : $"{Environment.NewLine}{exception}"));
+#endif
+        }
+    }
+}
---
+++ src/Universe.SqlInsights.Shared/CrossPlatformLogger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Universe.SqlInsights.Shared
+{
+    public interface ICrossPlatformLogger
+    {
+        public void Log(LogSeverity severity, string message, Exception exception);
+    }
+
+    public enum LogSeverity
+    {
+        Info,
+        Warning,
+        Error,
+    }
+
+    public static class CrossPlatformLoggerExtensions
+    {
+        public static void LogInformation(this ICrossPlatformLogger logger, string message)
+        {
+            logger?.Log(LogSeverity.Info, message, null);
+        }
+
+        public static void LogWarning(this ICrossPlatformLogger logger, Exception error, string message)
+        {
+            logger?.Log(LogSeverity.Warning, message, error);
+        }
+        public static void LogError(this ICrossPlatformLogger logger, Exception error, string message)
+        {
+            logger?.Log(LogSeverity.Error, message, error);
+        }
+    }
+
+    public class NullCrossPlatformLogger : ICrossPlatformLogger
+    {
+        public static readonly ICrossPlatformLogger Instance = new NullCrossPlatformLogger();
+
+        public void Log(LogSeverity severity, string message, Exception exception)
+        {
+        }
+    }
+
+
+}
