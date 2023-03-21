e4141d54ca21ff6b699cf996293d667087e31a83
devizer <devizer@users.github.io>
2023-03-21 19:57:21 +0200

    Adding Windows Nano Server Docker Images v8

--- scripts/Build-Windows-Docker-Image.ps1
+++ scripts/Build-Windows-Docker-Image.ps1
@@ -1,11 +1,31 @@
+function Get-Elapsed
+{
+    if ($Global:startAt -eq $null) { $Global:startAt = [System.Diagnostics.Stopwatch]::StartNew(); }
+    [System.String]::Concat("[", (new-object System.DateTime(0)).AddMilliseconds($Global:startAt.ElapsedMilliseconds).ToString("mm:ss"), "]");
+}; Get-Elapsed | out-null;
+
+function Say { param( [string] $message )
+    Write-Host "$(Get-Elapsed) " -NoNewline -ForegroundColor Magenta
+    Write-Host "$message" -ForegroundColor Yellow
+}
 
 $nanoVersions = $(
-  @{  TAG = "ltsc2022";
-      Version = "10.10.10.10"
-   }
+  @{  TAG = "ltsc2022"; Version = "10.0.20348.1607" },
+  @{  TAG = "20H2";     Version = "10.0.19042.1889" },
+  @{  TAG = "2004";     Version = "10.0.19041.1415" },
+  @{  TAG = "1909";     Version = "10.0.18363.1556" },
+  @{  TAG = "1903";     Version = "10.0.18362.1256" },
+  @{  TAG = "ltsc2019"; Version = "10.0.17763.4131" }, # 1809
+# @{  TAG = "1809";     Version = "10.0.17763.4131" }, # LTS 2019
+  @{  TAG = "1803";     Version = "10.0.17134.1305" },
+  @{  TAG = "1709";     Version = "10.0.16299.1087" }
 )
 
 pushd Windows-W3API-Docker
-$tag="ltsc2022"
-& docker build --build-arg TAG=$tag -t devizervlad/sqlinsights-dashboard-nanoserver:$tag .
+foreach($nanoVersion in $nanoVersions) {
+  $tag=$nanoVersion.Tag;
+  $ver=$nanoVersion.Version
+  Say "Building Tag '$tag', version $ver"
+  & docker build --build-arg TAG=$tag -t devizervlad/sqlinsights-dashboard-nanoserver:$tag .
+}
 popd
