e05aa7e68a7b19e2c5ac9fb4f9882051123d28d8
devizer <devizer@users.github.io>
2023-04-21 03:08:21 +0300

    [Dashboard], Added Auto Update Switch UI and "Live Update" label, Fix "On Selected Key Path Updated", v2

--- src/universe.sqlinsights.w3app/src/Actions/ActionList.js
+++ src/universe.sqlinsights.w3app/src/Actions/ActionList.js
@@ -2,9 +2,6 @@
 import React, { Component } from 'react';
 import dataSourceStore from "../stores/DataSourceStore";
 import {ActionKeyPathUi} from "../Shared/ActionKeyPathUi";
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 import ReactTable from "react-table";
 import "react-table/react-table.css";
 import {API_URL} from '../BuildTimeConfiguration';
