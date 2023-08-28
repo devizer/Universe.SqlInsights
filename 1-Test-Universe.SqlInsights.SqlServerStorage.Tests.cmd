543e80700601ea149e03ba34ca8bfb24a9f9fdbf
devizer <devizer@users.github.io>
2023-08-28 16:09:09 +0300

    Default startup project, bump up sdk for tests

--- 1-Test-Universe.SqlInsights.SqlServerStorage.Tests.cmd
+++ 1-Test-Universe.SqlInsights.SqlServerStorage.Tests.cmd
@@ -1,4 +1,4 @@
 pushd src\Universe.SqlInsights.SqlServerStorage.Tests
 rem dotnet test --collect:"XPlat Code Coverage" --logger trx
-dotnet test -c Release -f net5.0
+dotnet test -c Release -f net6.0
 popd
