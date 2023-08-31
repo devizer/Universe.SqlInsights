672d5db133e9896c08d4ef22202645261b21dd35
devizer <devizer@users.github.io>
2023-08-31 09:10:41 +0300

    Brush up headless test

--- src/AdventureWorks.HeadlessTests/ChromeDriverFactory.cs
+++ src/AdventureWorks.HeadlessTests/ChromeDriverFactory.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
 using Universe;
 using Universe.ChromeAndDriverInstaller;
