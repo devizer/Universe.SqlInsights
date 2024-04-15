33c6311954859612ded38ef61ea1edf981451076
devizer <devizer@users.github.io>
2024-04-15 22:54:34 +0300

    brush up

--- src/Universe.SqlInsights.W3Api/BoolConfigurationExtensions.cs
+++ src/Universe.SqlInsights.W3Api/BoolConfigurationExtensions.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 
-namespace Universe.SqlInsights.W3Api;
-
-public static class BoolConfigurationExtensions
+namespace Universe.SqlInsights.W3Api
 {
-    public static bool GetBooleanValue(this IConfiguration config, string configPath)
+    public static class BoolConfigurationExtensions
     {
-        var raw = config.GetValue<string>(configPath);
-        return
-            "True".Equals(raw, StringComparison.OrdinalIgnoreCase)
-            || "On".Equals(raw, StringComparison.OrdinalIgnoreCase)
-            || "1" == raw;
+        public static bool GetBooleanValue(this IConfiguration config, string configPath)
+        {
+            var raw = config.GetValue<string>(configPath);
+            return
+                "True".Equals(raw, StringComparison.OrdinalIgnoreCase)
+                || "On".Equals(raw, StringComparison.OrdinalIgnoreCase)
+                || "1" == raw;
+        }
+
     }
-        
 }
