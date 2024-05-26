5d1fc881b49b0d20879d861d9a224eca3d2376b7
devizer <devizer@users.github.io>
2024-05-27 02:22:47 +0300

    bump up dependencies (microsoft sqlclient and json), fix encryption options for tests

--- Dev-Env.cmd
+++ Dev-Env.cmd
@@ -1,5 +1,5 @@
 set SQLINSIGHTS_DATA_DIR=C:\SQL\SqlInsightsData
 setx SQLINSIGHTS_DATA_DIR C:\SQL\SqlInsightsData
 
-set SQLINSIGHTS_CONNECTION_STRING=Data Source=(local);Integrated Security=SSPI
-setx SQLINSIGHTS_CONNECTION_STRING "Data Source=(local);Integrated Security=SSPI"
+set SQLINSIGHTS_CONNECTION_STRING=Data Source=(local);Integrated Security=SSPI;TrustServerCertificate=True
+setx SQLINSIGHTS_CONNECTION_STRING "Data Source=(local);Integrated Security=SSPI;TrustServerCertificate=True"
