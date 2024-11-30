f4c80bc4c0be95b21ea6931b9abd722402542961
devizer <devizer@github.io>
2024-11-30 16:08:38 +0200

    Duplicate IdSession, Fix summary timestamp and delete session, ActionDetailsTestCase, 3 Universe.SqlInsights.W3Api.Client + Tests, PopulateSessionsCalculatedFields,  Brush up Universe.NUnitPipeline.SqlServerDatabaseFactory, Paddings for I/O pages+rows, Safe Download File Name 3rd, fix "ErgFab Tests Report.txt" v2, SqlServerTestCapturePipeline done (report on azure devops), 2nd ErgoFabSqlInsightsConfiguration done, new Universe.SqlInsights.NUnit, Universe.SqlInsights.NUnit --> Universe.NUnitPipeline.SqlServerDatabaseFactory , Fix Push pipeline (not enough space), Brush up, ErgoFab Tests on pipeline, Done Universe.SqlInsights.NUnit + clean up, Fix TestArgumentReflection, Bump up NUnit Pipeline, Removed IConfoguration from ErgoFab EF Model

---
+++ src/Universe.SqlInsights.W3Api.Client.Tests/ActionDetailsTestCase.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Slices.Dashboard;
+
+namespace Universe.SqlInsights.W3Api.Client.Tests
+{
+    public class ActionDetailsTestCase
+    {
+        public SqlInsightsSession Session { get; set; }
+        public long SessionId => Session?.IdSession ?? -42;
+        public string[] KeyPath { get; set; }
+
+        public override string ToString()
+        {
+            var ret = string.Join(" → ", KeyPath) + $" for session #{SessionId} '{Session.Caption}'";
+            return ret;
+        }
+
+        public static IEnumerable<ActionDetailsTestCase> GetTestCases()
+        {
+            S5ApiClient client = S5ApiClientFactory.Create();
+            ICollection<SqlInsightsSession> sessions = client.SessionsAsync().Result;
+            
+            sessions = sessions.Concat(new[] { new SqlInsightsSession() {IdSession = -42, Caption = "Fake Session"} }).ToList();
+
+            foreach (var session in sessions)
+            {
+                ActionsSummaryParameters request = new ActionsSummaryParameters()
+                {
+                    AppsFilter = null,
+                    HostsFilter = null,
+                    IdSession = session.IdSession,
+                };
+
+                ICollection<ActionSummaryCounters> actionsWithSummary = client.SummaryAsync(request).Result;
+                foreach (var action in actionsWithSummary)
+                {
+                    yield return new ActionDetailsTestCase()
+                    {
+                        Session = session,
+                        KeyPath = action.Key.Path.ToArray()
+                    };
+                }
+
+            }
+        }
+    }
+}
---
+++ src/Universe.SqlInsights.W3Api.Client.Tests/TestActionDetails.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Slices.Dashboard;
+
+namespace Universe.SqlInsights.W3Api.Client.Tests
+{
+    [TestFixture]
+    public class TestActionDetails
+    {
+        [Test]
+        [TestCaseSource(typeof(SessionsTestCase), nameof(SessionsTestCase.GetTestCases))]
+        public async Task TestSimple(SessionsTestCase testCase)
+        {
+            S5ApiClient client = S5ApiClientFactory.Create();
+
+            ActionsSummaryParameters request = new ActionsSummaryParameters()
+            {
+                AppsFilter = null,
+                HostsFilter = null,
+                IdSession = testCase.SessionId,
+            };
+
+            var actionsWithSummary = await client.SummaryAsync(request);
+            var firstPath = actionsWithSummary.FirstOrDefault()?.Key ?? new SqlInsightsActionKeyPath() { Path = new[] { "None" } };
+
+            ActionsParameters actionParameters = new ActionsParameters()
+            {
+                AppsFilter = null,
+                HostsFilter = null,
+                IdSession = testCase.SessionId,
+                Path = firstPath.Path
+            };
+            var details = await client.ActionsByKeyAsync(actionParameters);
+
+            Console.WriteLine($"Details for [{string.Join("→", firstPath.Path)}] (count={details.Count}){Environment.NewLine}{details.AsJson()}");
+        }
+
+
+        [Test]
+        [TestCaseSource(typeof(ActionDetailsTestCase), nameof(ActionDetailsTestCase.GetTestCases))]
+        public async Task TestFullActions(ActionDetailsTestCase testCase)
+        {
+            S5ApiClient client = S5ApiClientFactory.Create();
+
+
+            ActionsParameters actionParameters = new ActionsParameters()
+            {
+                AppsFilter = null,
+                HostsFilter = null,
+                IdSession = testCase.SessionId,
+                Path = testCase.KeyPath
+            };
+            var details = await client.ActionsByKeyAsync(actionParameters);
+
+            Console.WriteLine($"Details for [{string.Join("→", testCase.KeyPath)}] (count={details.Count}){Environment.NewLine}{details.AsJson()}");
+        }
+    }
+}
