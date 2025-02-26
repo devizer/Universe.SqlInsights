7ee930a3ae26e198642718d79ab4e67a95d5f739
devizer <devizer@github.io>
2025-02-26 05:49:52 +0200

    Build-NuGET-Array.ps1: Done, added Includes.ps1

--- scripts/Build-NuGET-Array.ps1
+++ scripts/Build-NuGET-Array.ps1
@@ -1,66 +1,34 @@
-$revision=203
+. .\Includes.ps1
+$NUnit_Pipeline_Revision=203
+$This_SqlIsnights_Version_Base="0.4.0"
 $nunit_versions = @(
-"3.7.0",
-"3.7.1",
-"3.8.0",
-"3.8.1",
-"3.9.0",
-"3.10.0",
-"3.10.1",
-"3.12.0",
-"3.13.0",
-"3.13.1",
-"3.13.2",
-"3.13.3",
-"3.14.0",
-"4.0.1",
-"4.1.0"
-"4.2.1",
-"4.2.2"
+  "3.7.0",
+  "3.7.1",
+  "3.8.0",
+  "3.8.1",
+  "3.9.0",
+  "3.10.0",
+  "3.10.1",
+  "3.12.0",
+  "3.13.0",
+  "3.13.1",
+  "3.13.2",
+  "3.13.3",
+  "3.14.0",
+  "4.0.1",
+  "4.1.0"
+  "4.2.1",
+  "4.2.2"
 )
+$Full_NUnit_Version = "3.14.0"
+
+
 
 # $nunit_versions = @("3.13.2", "4.2.2");
 $projects = @("Universe.NUnitPipeline.SqlServerDatabaseFactory", "Universe.SqlInsights.NUnit");
 
-function Get-Commit-Count() {
-  $commitsRaw = & { set TZ=GMT; git log -999999 --date=raw --pretty=format:"%cd" }
-  $lines = $commitsRaw.Split([Environment]::NewLine)
-  $commitCount = $lines.Length
-  return $commitCount
-}
-function Get-Elapsed { 
-    if ($Global:_Say_Stopwatch -eq $null) { $Global:_Say_Stopwatch = [System.Diagnostics.Stopwatch]::StartNew(); }
-    $milliSeconds=$Global:_Say_Stopwatch.ElapsedMilliseconds
-    if ($milliSeconds -ge 3600000) { $format="HH:mm:ss"; } else { $format="mm:ss"; }
-    return "[$((new-object System.DateTime(0)).AddMilliseconds($milliSeconds).ToString($format))]"
-}; $Global:_Say_Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-
-function Set-Target-Version-for-NUnit-Version([string] $NUnit_Version) {
-  if ($NUnit_Version -like "3.7.*" -or $NUnit_Version -like "3.8.*" -or $NUnit_Version -like "3.9.*") {
-      # the latest net20
-      $forced="netstandard2.0;net462;net46"
-      $GLOBAL:TARGET_FRAMEWORKS_LIB="netstandard1.3;netstandard1.6;net35;net40;net45;$forced"
-      $GLOBAL:TARGET_FRAMEWORKS_TEST="netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net40;net45;net462;net48;net8.0"
-  }
-  elseif ($NUnit_Version -like "3.10.*") {
-      # the latest net20
-      $forced="net462;net46"
-      $GLOBAL:TARGET_FRAMEWORKS_LIB="netstandard1.6;netstandard2.0;net35;net4.0;net45;$forced"
-      $GLOBAL:TARGET_FRAMEWORKS_TEST="netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net40;net45;net462;net48;net8.0"
-  }
-  elseif ($NUnit_Version -like "3.*") {
-      $forced="net46;net462"
-      $GLOBAL:TARGET_FRAMEWORKS_LIB="netstandard2.0;net35;net40;net45;$forced"
-      $GLOBAL:TARGET_FRAMEWORKS_TEST="net8.0;net6.0;netcoreapp3.1;net48;net462;net45;net40;net35"
-  }
-  else {
-      # v4.x
-      $GLOBAL:TARGET_FRAMEWORKS_LIB="net462;net6.0";
-      $GLOBAL:TARGET_FRAMEWORKS_TEST="net8.0;net6.0;net48;net462";
-  }
-}
-
 $Commit_Count = Get-Commit-Count
+$This_SqlIsnights_Version = "$This_SqlIsnights_Version_Base.$Commit_Count"
 write-host "Commit Count: $Commit_Count"
 
 $Work_Base="W:\Temp\Universe.SqlInsights-NUnit-Packages"
@@ -68,6 +36,12 @@ Remove-Item -Recurse -Force "$Work_Base" -EA SilentlyContinue | Out-Null
 New-Item "$Work_Base" -Force -ItemType Container -EA SilentlyContinue | Out-Null
 & git.exe clone https://github.com:/devizer/Universe.SqlInsights "$Work_Base\base"
 # Remove-Item -Recurse -Force "$Work_Base\base\.git" -EA SilentlyContinue | Out-Null
+
+$csprojs = @(Get-ChildItem -Path "$Work_Base" -Filter "*.csproj" -Recurse)
+foreach($csproj in $csprojs) {
+  Write-Host "Patch $($csproj.FullName) as version $($This_SqlIsnights_Version)"
+  Set-CS-Project-Version "$($csproj.FullName)" "$This_SqlIsnights_Version"
+}
 
 
 $buildIndex = 0;
@@ -80,8 +54,8 @@ foreach($NUnit_Version in $nunit_versions) {
   foreach($project in $projects) {
     $buildIndex++;
     Write-Host "$(Get-Elapsed) $BuildIndex of $($buildCount): $nunit_Version $project" -ForegroundColor Magenta
-    $This_Version = "$NUnit_Version.$Commit_Count"
-    write-host "THIS VERSION: $this_version"
+    $This_NUnit_Version = "$NUnit_Version.$Commit_Count"
+    write-host "THIS VERSION: $This_NUnit_Version"
 
     Set-Target-Version-for-NUnit-Version $NUnit_Version
     Write-Host "TARGET_FRAMEWORKS_LIB:  $($TARGET_FRAMEWORKS_LIB)" -ForegroundColor Magenta
@@ -90,8 +64,13 @@ foreach($NUnit_Version in $nunit_versions) {
     pushd src\$project
     & C:\Apps\Git\usr\bin\sed.exe "-i", "-E", "s|<TargetFrameworks>.*</TargetFrameworks>|<TargetFrameworks>$TARGET_FRAMEWORKS_LIB</TargetFrameworks>|" "$($project).csproj"
     & dotnet remove package Universe.NUnitPipeline
-    & dotnet add package Universe.NUnitPipeline -v "$nunit_Version.$revision"
-    & { dotnet "build", "-c", "Release", "-p:PackageVersion=$This_Version", "-p:Version=$This_Version" } *| tee "..\..\..\$nunit_Version-$($project)-build.log"
+    & dotnet add package Universe.NUnitPipeline -v "$nunit_Version.$NUnit_Pipeline_Revision"
+    Set-CS-Project-Version "$PWD\$($project).csproj" "$This_NUnit_Version"
+    & { dotnet "build", "-c", "Release" 2>&1 } *| tee "..\..\..\$nunit_Version-$($project)-build.log"
+    if ($nunit_Version -eq $Full_NUnit_Version) {
+      cd ..
+      & dotnet build "-c" Release
+    }
     popd
     Write-Host ""
   } 
@@ -102,3 +81,7 @@ Write-Host "$(Get-Elapsed) Finish" -ForegroundColor Magenta
 $nupkgs = @(Get-ChildItem -Path "$Work_Base" -Filter "*.nupkg" -Recurse)
 Write-Host "Copying $($nupkgs.Length) nupkg-files"
 $nupkgs | Copy-Item -Destination "$Work_Base\"
+
+Write-Host "Remidned: Do NOT publish [Universe.SqlInsights.W3Api.Client.*.nupkg]" -ForegroundColor Yellow
+
+
---
+++ scripts/Includes.ps1
@@ -0,0 +1,72 @@
+function Set-CS-Project-Version([string] $csproj, [string]$version) {
+  $raw = Get-Content -Raw $csproj
+  if ($raw.IndexOf("http://schemas.microsoft.com/developer/msbuild/2003") -ge 0) { return; }
+  $lines = @(Get-Content $csproj)
+  $lines = @($lines | ? { -not ($_ -match "<PackageVersion>" -or $_ -match "<Version>") })
+  $newLine = "<!-- Auto Generated --> <PropertyGroup><PackageVersion>$version</PackageVersion><Version>$version</Version></PropertyGroup> <!-- Auto Generated -->"
+  $newLines = @($lines[0]) + @($newLine) + @($lines[1..($lines.Count-1)])
+  Write-All-Text "$csproj" ($newLines -join "`r`n")
+  # Write-Host "[$($csproj)] at [$PWD]`r`n $($newLines -join "`r`n")"
+}
+
+function Create-Directory($dirName) { 
+  if ($dirName) { 
+    $err = $null;
+    try { 
+      $_ = [System.IO.Directory]::CreateDirectory($dirName); return; 
+    } catch {
+      $err = "Create-Directory failed for `"$dirName`". $($_.Exception.GetType().ToString()) $($_.Exception.Message)"
+      Write-Host "Warning! $err";
+      throw $err;
+    }
+  }
+}
+
+function Create-Directory-for-File($fileFullName) { 
+  $dirName=[System.IO.Path]::GetDirectoryName($fileFullName)
+  Create-Directory "$dirName";
+}
+
+function Write-All-Text( [string]$file, [string]$text ) {
+  Create-Directory-for-File $file
+  $utf8=new-object System.Text.UTF8Encoding($false); 
+  [System.IO.File]::WriteAllText($file, $text, $utf8);
+}
+function Get-Commit-Count() {
+  $commitsRaw = & { set TZ=GMT; git log -999999 --date=raw --pretty=format:"%cd" }
+  $lines = $commitsRaw.Split([Environment]::NewLine)
+  $commitCount = $lines.Length
+  return $commitCount
+}
+function Get-Elapsed { 
+    if ($Global:_Say_Stopwatch -eq $null) { $Global:_Say_Stopwatch = [System.Diagnostics.Stopwatch]::StartNew(); }
+    $milliSeconds=$Global:_Say_Stopwatch.ElapsedMilliseconds
+    if ($milliSeconds -ge 3600000) { $format="HH:mm:ss"; } else { $format="mm:ss"; }
+    return "[$((new-object System.DateTime(0)).AddMilliseconds($milliSeconds).ToString($format))]"
+}; $Global:_Say_Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+
+function Set-Target-Version-for-NUnit-Version([string] $NUnit_Version) {
+  if ($NUnit_Version -like "3.7.*" -or $NUnit_Version -like "3.8.*" -or $NUnit_Version -like "3.9.*") {
+      # the latest net20
+      $forced="netstandard2.0;net462;net46;net48;net8.0"
+      $GLOBAL:TARGET_FRAMEWORKS_LIB="netstandard1.3;netstandard1.6;net35;net40;net45;$forced"
+      $GLOBAL:TARGET_FRAMEWORKS_TEST="netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net40;net45;net462;net48;net8.0"
+  }
+  elseif ($NUnit_Version -like "3.10.*") {
+      # the latest net20
+      $forced="net462;net46;net48;net8.0"
+      $GLOBAL:TARGET_FRAMEWORKS_LIB="netstandard1.6;netstandard2.0;net35;net40;net45;$forced"
+      $GLOBAL:TARGET_FRAMEWORKS_TEST="netcoreapp1.0;netcoreapp1.1;netcoreapp2.0;netcoreapp2.1;netcoreapp3.0;netcoreapp3.1;net20;net35;net40;net45;net462;net48;net8.0"
+  }
+  elseif ($NUnit_Version -like "3.*") {
+      $forced="net46;net462;net48;net8.0"
+      $GLOBAL:TARGET_FRAMEWORKS_LIB="netstandard2.0;net35;net40;net45;$forced"
+      $GLOBAL:TARGET_FRAMEWORKS_TEST="net8.0;net6.0;netcoreapp3.1;net48;net462;net45;net40;net35"
+  }
+  else {
+      # v4.x
+      $forced="net46;net462;net48;net8.0"
+      $GLOBAL:TARGET_FRAMEWORKS_LIB="net462;net6.0";
+      $GLOBAL:TARGET_FRAMEWORKS_TEST="net8.0;net6.0;net48;net462";
+  }
+}
