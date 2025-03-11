d375196a5ba14a3f3e14946c7c2bc741132d38e1
devizer <devizer@github.io>
2025-03-11 03:02:10 +0200

    7 Widows + Linux pipelines: stress via stored procs: Insert, Select, and Delete

--- scripts/Load-SQL-Server-by-Stored-Procedure.ps1
+++ scripts/Load-SQL-Server-by-Stored-Procedure.ps1
@@ -1,4 +1,4 @@
-$instance="(local)"
+﻿$instance="(local)"
 $connectionString="Data Source=$instance; Integrated Security=SSPI;Encrypt=False"
 if ("$($ENV:STRESS_CONNECTION_STRING)" -ne "") { $connectionString = "$($ENV:STRESS_CONNECTION_STRING)"; }
 Write-Host "Invoke a commands set on instance '$i'"
@@ -9,16 +9,16 @@ $migrate=@(
  "Create Database [DB-Stress];",
  "Use [DB-Stress]; Create Table Detail(Id int identity, Description nvarchar(max), Constraint PK_Detail Primary Key (Id));",
  "Use [DB-Stress]; EXEC('Create Procedure [Stress By Insert] As Begin Declare @desc char(4000); Set @desc = ''A Description''; Insert Detail(Description) Values(@desc); End')",
- "Use [DB-Stress]; EXEC('Create Procedure [Stress By Select] As Begin Select Top 200 Id, Description From Detail Order By Id Desc; End')"
+ "Use [DB-Stress]; EXEC('Create Procedure [Stress By Select] As Begin Select Top 200 Id, Description From Detail Order By Id Desc; End')",
  "Use [DB-Stress]; EXEC('Create Procedure [Stress By Delete] As Begin Delete From Detail Where Id = (Select MAX(Id) From Detail); End')"
 );
 $insert="Use [DB-Stress]; Set NoCount On; Declare @i int; Set @i = 0; While @i < 10000 Begin Exec [Stress By Insert]; Set @i = @i + 1; End;";
 $select="Use [DB-Stress]; Set NoCount On; Declare @i int; Set @i = 0; While @i < 50    Begin Exec [Stress By Select]; Set @i = @i + 1; End;";
 $delete="Use [DB-Stress]; Set NoCount On; Declare @i int; Set @i = 0; While @i < 1000  Begin Exec [Stress By Delete]; Set @i = @i + 1; End;";
 
-$commands=$migrate + @($ins, $select, $ins, $select, $ins, $select, $ins, $select);
+$commands=$migrate + @($insert, $select, $delete, $insert, $select, $insert, $select, $insert, $delete, $select);
 
 foreach($cmd in $commands) { 
-  Write-Host "Invoke: $cmd"; 
+  Write-Host "Invoke: «$cmd»"; 
   Measure-Action "Stress" { Invoke-SqlServer-Command -Title "Instance" -ConnectionString "$connectionString" -SqlCommand $cmd; }
 }
