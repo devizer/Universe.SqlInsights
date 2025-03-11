597346636d5281d8149249001b837fd1513776bb
devizer <devizer@github.io>
2025-03-11 02:24:21 +0200

    4 Widows + Linux pipelines: stress via stored procs: Insert, Select, and Delete

--- scripts/Load-SQL-Server-by-Stored-Procedure.ps1
+++ scripts/Load-SQL-Server-by-Stored-Procedure.ps1
@@ -5,14 +5,16 @@ Write-Host "Invoke a commands set on instance '$i'"
 
 # 30000 to fit buffer page into 1.4 Gb of RAM
 $migrate=@(
- "If Exists(Select 1 From sys.databases where name='DBTest') Drop Database DBTest;",
- "Create Database DBTest;",
- "Use DBTest; Create Table Detail(Id int identity, Description nvarchar(max), Constraint PK_Detail Primary Key (Id));",
- "Use DBTest; EXEC('Create Procedure [Stress By Insert] As Begin Declare @desc char(4000); Set @desc = ''A Description''; Insert Detail(Description) Values(@desc); End')",
- "Use DBTest; EXEC('Create Procedure [Stress By Select] As Begin Select Top 2000 Id, Description From Detail Order By Id Desc; End')"
+ "If Exists(Select 1 From sys.databases where name='DB-Stress') Drop Database [DB-Stress];",
+ "Create Database [DB-Stress];",
+ "Use [DB-Stress]; Create Table Detail(Id int identity, Description nvarchar(max), Constraint PK_Detail Primary Key (Id));",
+ "Use [DB-Stress]; EXEC('Create Procedure [Stress By Insert] As Begin Declare @desc char(4000); Set @desc = ''A Description''; Insert Detail(Description) Values(@desc); End')",
+ "Use [DB-Stress]; EXEC('Create Procedure [Stress By Select] As Begin Select Top 200 Id, Description From Detail Order By Id Desc; End')"
+ "Use [DB-Stress]; EXEC('Create Procedure [Stress By Delete] As Begin Delete From Detail Where Id = (Select MAX(Id) From Detail); End')"
 );
 $ins="Use DBTest; Set NoCount On; Declare @i int; Set @i = 0; While @i < 10000 Begin Exec [Stress By Insert]; Set @i = @i + 1; End;";
 $select="Use DBTest; Set NoCount On; Declare @i int; Set @i = 0; While @i < 50 Begin Exec [Stress By Select]; Set @i = @i + 1; End;";
+$delete="Use DBTest; Set NoCount On; Declare @i int; Set @i = 0; While @i < 1000 Begin Exec [Stress By Delete]; Set @i = @i + 1; End;";
 
 $commands=$migrate + @($ins, $select, $ins, $select, $ins, $select, $ins, $select);
 
