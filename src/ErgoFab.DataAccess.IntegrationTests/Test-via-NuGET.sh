7206a1dbba37dbf9ad4bc8915fb8a0e402362ad2
devizer <devizer@github.io>
2025-04-02 09:39:36 +0300

    Linux Pipeline: Test ErgoFab via Nuget Packages and Latest Version

--- src/ErgoFab.DataAccess.IntegrationTests/Test-via-NuGET.sh
+++ src/ErgoFab.DataAccess.IntegrationTests/Test-via-NuGET.sh
@@ -1,5 +1,16 @@
+function find-build-number() {
+  pushd /tmp >/dev/null
+  dotnet new console -o console1; cd console1 >/dev/null
+  dotnet add package Universe.SqlInsights.NUnit >/dev/null
+  cat *.csproj | grep PackageReference | grep -Eo "[0-9]{1,7}" | tail -1
+  cd ..
+  rm -rf console1 >/dev/null 2>&1
+  popd >/dev/null
+}
 prj=ErgoFab.DataAccess.IntegrationTests.csproj
-ver="3.14.0.1119"
+build_number="$(find-build-number)"
+echo "BUILD NUMBER = [$build_number]"
+ver="3.14.0.$build_number"
 sed -i '/Universe.NUnitPipeline.SqlServerDatabaseFactory.csproj/d' "$prj"
 sed -i '/Universe.SqlInsights.NUnit.csproj/d' "$prj"
 dotnet add "$prj" package Universe.NUnitPipeline.SqlServerDatabaseFactory -v "$ver"
