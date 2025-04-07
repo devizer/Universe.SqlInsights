7db7c8531a9cc94c2fb466f7cbd43fce26d6e780
devizer <devizer@github.io>
2025-04-07 07:23:45 +0300

    8th nuget packages: Try & Retry, Brush up pipeline

--- scripts/Build-NuGET-Array.ps1
+++ scripts/Build-NuGET-Array.ps1
@@ -93,7 +93,7 @@ foreach($NUnit_Version in $nunit_versions) {
     Try-And-Retry "Build $project $This_NUnit_Version" { & { dotnet "build", "-c", "Release" 2>&1 } *| tee "..\..\..\$nunit_Version-$($project)-build.log" }
     if ($nunit_Version -eq $Full_NUnit_Version) {
       cd ..
-      & dotnet build "-c" Release
+      # & dotnet build "-c" Release
     }
     popd
     Write-Host ""
