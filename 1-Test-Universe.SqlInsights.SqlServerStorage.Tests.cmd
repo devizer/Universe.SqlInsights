cabe666d25327a0f83d832abb6b1a433971fe81c
devizer <devizer@github.io>
2025-05-09 13:54:22 +0300

    Bump up Universe.SqlServerJam, netstandard 1.3 --> 1.4

--- 1-Test-Universe.SqlInsights.SqlServerStorage.Tests.cmd
+++ 1-Test-Universe.SqlInsights.SqlServerStorage.Tests.cmd
@@ -1,4 +1,15 @@
 pushd src\Universe.SqlInsights.SqlServerStorage.Tests
 rem dotnet test --collect:"XPlat Code Coverage" --logger trx
 dotnet test -c Release -f net6.0
+If ErrorLevel 1 Goto :error
 popd
+goto :finish
+
+:error
+echo ERROR!!! Universe.SqlInsights.SqlServerStorage.Tests
+echo ERROR!!! Universe.SqlInsights.SqlServerStorage.Tests
+echo ERROR!!! Universe.SqlInsights.SqlServerStorage.Tests
+echo ERROR!!! Universe.SqlInsights.SqlServerStorage.Tests
+exit 77
+
+:finish
--- Z-All.cmd
+++ Z-All.cmd
@@ -2,6 +2,7 @@ call 0a-Net-Restore-All.cmd
 call 0b-Net-Rebuild-All.cmd 
 call 1-Run-AdventureWorks.cmd 
 call 1-Run-W3Api.cmd 
+call 1-Test-Universe.SqlInsights.SqlServerStorage.Tests.cmd 
 call 2c-Serve-SqlInsights-w3app.cmd 
 call 3-Run-AdventureWorks-Headless-Test.cmd 
 start 1b-FakeError.html
