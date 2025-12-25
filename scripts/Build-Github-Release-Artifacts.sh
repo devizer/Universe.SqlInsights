12488f76618e26271f1126e6c692e33e938434b8
devizer <devizer@github.io>
2025-12-25 16:57:56 +0200

    9th Migration to v10.0 ***NO_CI***

--- scripts/Build-Github-Release-Artifacts.sh
+++ scripts/Build-Github-Release-Artifacts.sh
@@ -74,9 +74,10 @@ for kind in RELEASE LEGACY; do
       time 7z a -t7z -mx=${COMPRESSION_LEVEL} -ms=on -mqs=on "$public"/$prefix-fxdependent${SUFFIX}.7z * | Filter-7z
     popd
 
+    rids_count="$(echo $RIDS | wc -w)"
     for r in $RIDS; do
       n=$((n+1))
-      Say "#${n}: BUILD SELF-CONTAINED [$r] $SQLINSIGHTS_VERSION"
+      Say "#${n}/${rids_count}: BUILD SELF-CONTAINED [$r] $SQLINSIGHTS_VERSION, kind = [$kind]"
       df -h -T
       try-and-retry $DOTNET_TARGET_DIR/dotnet publish --self-contained -r $r -f net$NET -o bin/plain/$r${SUFFIX} -v:q -p:Version=$SQLINSIGHTS_VERSION_SHORT -c Release
       mkdir -p bin/plain/$r${SUFFIX}/wwwroot; cp -r -a "$BUILD_REPOSITORY_LOCALPATH/src/universe.sqlinsights.w3app/build"/. bin/plain/$r${SUFFIX}/wwwroot
