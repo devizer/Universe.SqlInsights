5b0491ba57e7ca2c8c571010b935f7429dd8e0e9
devizer <devizer@users.github.io>
2026-02-09 02:07:32 +0200

    Refactoring AZ Pipeline [skip ci]

--- scripts/CI-Steps/1-Install-Module.ps1
+++ scripts/CI-Steps/1-Install-Module.ps1
@@ -1,5 +1,10 @@
 Write-Host "Location: $(Get-Location)"
 
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+if ($PSVersionTable.PSEdition -ne "Core") {
+  [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true};
+}
+
 $urlSource = 'https://devizer.github.io/SqlServer-Version-Management/Install-SqlServer-Version-Management.ps1'; 
 foreach($attempt in 1..3) { try { iex ((New-Object System.Net.WebClient).DownloadString($urlSource)); Write-Host "Success: Install-SqlServer-Version-Management.ps1"; break; } catch {sleep 0.1;} }
 
