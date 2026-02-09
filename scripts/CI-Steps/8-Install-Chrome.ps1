f2e929921c3e9094f6f7e5f22081b21b4820e20b
devizer <devizer@users.github.io>
2026-02-10 01:27:32 +0200

    3300M RAM, Stop on error, Faster Tests: 1) Hiding Preinstalled LocalDB, 2) Tuning Scalability Tests [skip ci]

---
+++ scripts/CI-Steps/8-Install-Chrome.ps1
@@ -0,0 +1,16 @@
+Import-DevOps
+Say "Installing chrome ..."
+choco install googlechrome
+
+Say "CONTENT: C:\Program Files\Google\Chrome\Application"
+Get-ChildItem "C:\Program Files\Google\Chrome\Application" | format-table -autosize
+
+Say "Assing chrome for http://"
+# reg add "HKEY_CLASSES_ROOT\http\shell\open\command" /ve /t REG_SZ /d "\"C:\Program Files\Google\Chrome\Application\chrome.exe\" --headless --no-sandbox --disable-gpu \"%%1\"" /f
+& reg.exe add "HKEY_CLASSES_ROOT\http\shell\open\command" /ve /t REG_SZ /d '\"C:\Program Files\Google\Chrome\Application\chrome.exe\" --headless --no-sandbox --disable-gpu \"%1\"' /f
+
+Say "Installing dotnet serve ..."
+& dotnet tool install --global dotnet-serve
+
+# ENV PATH="${PATH};C:\Users\ContainerUser\.dotnet\tools;C:\Program Files\Google\Chrome\Application"
+# RUN reg add "HKEY_CLASSES_ROOT\http\shell\open\command" /ve /t REG_SZ /d "\"C:\Program Files\Google\Chrome\Application\chrome.exe\" --headless --no-sandbox --disable-gpu \"%%1\"" /f
