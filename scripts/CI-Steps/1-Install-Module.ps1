93ff9e507d92b11af0bcfb081f59afbc2d9bc468
devizer <devizer@users.github.io>
2026-02-09 02:05:20 +0200

    Refactoring AZ Pipeline [skip ci]

--- scripts/CI-Steps/1-Install-Module.ps1
+++ scripts/CI-Steps/1-Install-Module.ps1
@@ -1,5 +1,6 @@
 Write-Host "Location: $(Get-Location)"
-$urlSource = 'https://devizer.github.io/SqlServer-Version-Management/SqlServer-Version-Management.ps1'; 
-foreach($attempt in 1..3) { try { iex ((New-Object System.Net.WebClient).DownloadString($urlSource)); break; } catch {sleep 0.1;} }
+
+$urlSource = 'https://devizer.github.io/SqlServer-Version-Management/Install-SqlServer-Version-Management.ps1'; 
+foreach($attempt in 1..3) { try { iex ((New-Object System.Net.WebClient).DownloadString($urlSource)); Write-Host "Success: Install-SqlServer-Version-Management.ps1"; break; } catch {sleep 0.1;} }
 
 Get-ChildItem | Format-Table -Autosize
