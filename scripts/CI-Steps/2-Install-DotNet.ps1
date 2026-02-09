44f7356d64639497df724e4ed0171a2a66756eee
devizer <devizer@users.github.io>
2026-02-09 13:02:03 +0200

    Try docker [skip ci]

--- scripts/CI-Steps/2-Install-DotNet.ps1
+++ scripts/CI-Steps/2-Install-DotNet.ps1
@@ -1,7 +1,14 @@
 Import-DevOps
 
+pushd $ENV:USERPROFILE
+Download-File-Managed https://dot.net/v1/dotnet-install.ps1 ".\dotnet-install.ps1"
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+if ($PSVersionTable.PSEdition -ne "Core") {
+  [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true};
+}
+
 foreach($ver in @("6.0", "8.0", "10.0")) {
-  Run-Remote-Script https://dot.net/v1/dotnet-install.ps1 -Channel "$ver" -InstallDir "C:\Program Files\dotnet"
+  . .\dotnet-install.ps1 -Channel "$ver" -InstallDir "C:\Program Files\dotnet"
 }
 
 Add-Folder-To-System-Path "C:\Program Files\dotnet"
@@ -11,3 +18,5 @@ New-item "$tools_folder" -ItemType Directory -Force -EA SilentlyContinue | Out-N
 Add-Folder-To-System-Path "$tools_folder"
 
 dotnet --info
+
+popd
