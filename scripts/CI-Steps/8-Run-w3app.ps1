0fa6d52df2cf5d9c15d9c8a5c509848a4fb00d08
devizer <devizer@users.github.io>
2026-02-09 03:04:14 +0200

    Refactoring AZ Pipeline [skip ci]

---
+++ scripts/CI-Steps/8-Run-w3app.ps1
@@ -0,0 +1,22 @@
+cd C:\App\Goods\w3api\wwwroot
+Say "CONTENT FOR $(Get-Location)"
+Get-ItemContent | format-table
+
+Say "Installing dotnet serve"
+dotnet tool install --global dotnet-serve
+
+Say "About dotnet serve"
+dotnet serve --version 2>$null
+
+Say "LAUNCHING"
+cmd /c "start dotnet serve -p 6060"
+sleep 1
+
+Say "PROCESSES"
+Get-Process -Name "dotnet-serve" | format-table -autosize | out-host
+
+
+Say "Validate http connection to http://localhost:6060"
+& curl.exe -I http://localhost:6060
+
+$true
