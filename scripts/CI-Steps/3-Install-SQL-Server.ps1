3c4e83a13693c94c6033a30d2f20475ba95d7478
devizer <devizer@users.github.io>
2026-02-09 05:20:08 +0200

    Refactoring AZ Pipeline [skip ci]

--- scripts/CI-Steps/3-Install-SQL-Server.ps1
+++ scripts/CI-Steps/3-Install-SQL-Server.ps1
@@ -1,5 +1,10 @@
 Import-DevOps
 
+setx PS1_TROUBLE_SHOOT "On"
+setx SQLSERVERS_SETUP_FOLDER "C:\SQL-Setup"
+$ENV:PS1_TROUBLE_SHOOT="On"
+$ENV:SQLSERVERS_SETUP_FOLDER="C:\SQL-Setup"
+
 Run-Remote-Script https://raw.githubusercontent.com/devizer/Universe.SqlServerJam/master/SQL-Server-in-Windows-Container/Setup-SQL-Server-in-Container.ps1 *>&1 | Tee-Object -FilePath "C:\App\SETUP-SQL-SERVER-OUTPUT.TXT"
 
 if ("$ENV:SQL" -match "2005") {
