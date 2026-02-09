5648a78409a4dca6c101e37545d723bde556abce
devizer <devizer@users.github.io>
2026-02-09 15:36:05 +0200

    Refactoring AZ Pipeline [skip ci]

--- scripts/CI-Steps/2-Install-DotNet.ps1
+++ scripts/CI-Steps/2-Install-DotNet.ps1
@@ -1,8 +1,9 @@
 Import-DevOps
 
-pushd "$ENV:USERPROFILE"
-Download-File-Managed https://dot.net/v1/dotnet-install.ps1 ".\dotnet-install.ps1"
+# pushd "$ENV:USERPROFILE"
+
 <#
+Download-File-Managed https://dot.net/v1/dotnet-install.ps1 ".\dotnet-install.ps1"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 if ($PSVersionTable.PSEdition -ne "Core") {
   [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true};
@@ -11,15 +12,15 @@ if ($PSVersionTable.PSEdition -ne "Core") {
 
 # 2016 by default TLS is not 1.2
 foreach($ver in @("6.0", "8.0", "10.0")) {
-  . .\dotnet-install.ps1 -Channel "$ver" -InstallDir "C:\Program Files\dotnet"
+  Run-Remote-Script https://dot.net/v1/dotnet-install.ps1 -Channel "$ver" -InstallDir "C:\Program Files\dotnet"
 }
 
 Add-Folder-To-System-Path "C:\Program Files\dotnet"
 
 $tools_folder="$($ENV:USERPROFILE)\.dotnet\tools"
 New-item "$tools_folder" -ItemType Directory -Force -EA SilentlyContinue | Out-Null
-Add-Folder-To-System-Path "$tools_folder"
+Add-Folder-To-User-Path "$tools_folder"
 
 dotnet --info
 
-popd
+# popd
