db4ea656045a5ae74b41041696a88ac64e04ae98
devizer <devizer@users.github.io>
2026-02-10 23:50:47 +0200

    Fix 9-Run-w3app.ps1 Refactoring AZ Pipeline [skip ci]

--- scripts/CI-Steps/1-Install-Bash-from-Git.ps1
+++ scripts/CI-Steps/1-Install-Bash-from-Git.ps1
@@ -1,3 +1,4 @@
+echo "`$PSNativeCommandArgumentPassing = [$PSNativeCommandArgumentPassing]"
 & choco feature enable -n allowGlobalConfirmation
 & choco feature disable -n showDownloadProgress
 choco install git
@@ -6,3 +7,4 @@ Add-Folder-To-System-Path "C:\Program Files\Git\bin"
 setx PS1_TROUBLE_SHOOT "On"
 setx SQLSERVERS_SETUP_FOLDER "C:\SQL-Setup"
 setx PS1_REPO_DOWNLOAD_FOLDER "C:\Temp\DevOps"
+
