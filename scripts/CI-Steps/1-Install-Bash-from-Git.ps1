e29b81246b35579cce572cee53f2c94b05bc6835
devizer <devizer@users.github.io>
2026-02-10 22:52:34 +0200

    3300M RAM, Stop on error, Faster Tests: 1) Hiding Preinstalled LocalDB, 2) Tuning Scalability Tests [skip ci]

--- scripts/CI-Steps/1-Install-Bash-from-Git.ps1
+++ scripts/CI-Steps/1-Install-Bash-from-Git.ps1
@@ -3,3 +3,6 @@
 choco install git
 Add-Folder-To-System-Path "C:\Program Files\Git\bin"
 
+setx PS1_TROUBLE_SHOOT "On"
+setx SQLSERVERS_SETUP_FOLDER "C:\SQL-Setup"
+setx PS1_REPO_DOWNLOAD_FOLDER "C:\Temp\DevOps"
