8b8742c6c877ec15b4fc26705ccddc51cbc1faff
devizer <devizer@users.github.io>
2026-02-11 01:11:56 +0200

    Fix 9-Run-w3app.ps1 Refactoring AZ Pipeline [skip ci]

--- scripts/CI-Steps/10-Run-w3api.ps1
+++ scripts/CI-Steps/10-Run-w3api.ps1
@@ -14,6 +14,20 @@ cd C:\App\Goods\w3api
 $ENV:ASPNETCORE_URLS="http://*:50420"
 # Start-Process "dotnet" -ArgumentList "Universe.SqlInsights.W3Api.dll".Split(" ") -NoNewWindow
 # & { dotnet Universe.SqlInsights.W3Api.dll } &
+# $ENV:LocalLogsFolder__Windows = "C:\Artifacts\Api-Logs"
+# $ENV:LocalLogsFolder__Enable = "True"
 Smart-Start-Process "dotnet" "Universe.SqlInsights.W3Api.dll"
 
 Sleep 60
+
+Select-WMI-Objects Win32_Process | Select-Object ProcessId, Name, @{Name="WS(MB)"; Expression={[math]::Round($_.WorkingSetSize / 1MB, 1)}}, CommandLine | Sort-Object ProcessId | ft -AutoSize | Out-String -width 200
+
+# SHOW Logs
+$logsFolder = "$($ENV:SystemDrive)\\Temp\\SqlInsights Dashboard Logs"
+$logsExists = [bool] (Test-Path $logsFolder)
+Say "W3API logsFolder = [$logsFolder]"
+Say "W3API logsExists = [$logsExists]"
+if ($logsExists) { Get-ChildItem $logsFolder | Format-Table -AutoSize }
+
+
+
