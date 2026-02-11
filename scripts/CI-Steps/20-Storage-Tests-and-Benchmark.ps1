725d35976cbe8ef1dfef1a8dd2ae3f8494f813ac
devizer <devizer@users.github.io>
2026-02-11 15:07:04 +0200

    Latest Chrome Fix, Fix 9-Run-w3app.ps1 Refactoring AZ Pipeline [skip ci]

---
+++ scripts/CI-Steps/20-Storage-Tests-and-Benchmark.ps1
@@ -0,0 +1,23 @@
+. "$PSScriptRoot\Functions.ps1"
+
+      cd C:\App\Goods\w3api.tests
+
+      Say "Full Storage Test With Coverage"
+      if (-not "$ENV:RAM_DISK") { $ENV:NUNIT_PIPELINE_KEEP_TEMP_TEST_DATABASES = "True" } # for query cache
+      $data_dir="C:\SQL-Data"
+      if (Test-Path "D:\") { $data_dir="D:\SQL-Data" }
+      $ENV:SQLINSIGHTS_DATA_DIR = "$data_dir"
+      $ENV:TEST_CPU_NAME = "$(Get-Cpu-Name)"
+      $ENV:TEST_CONFIGURATION = "DISK";
+      if ("$ENV:DB_DATA_DIR") {
+        $ENV:SQLINSIGHTS_DATA_DIR = "$ENV:DB_DATA_DIR"
+        $ENV:TEST_CONFIGURATION = "RAM-Disk"
+        Say "SQLINSIGHTS_DATA_DIR is $ENV:SQLINSIGHTS_DATA_DIR"
+      fi
+      $ENV:TEST_UNDER_COVERAGE = "True"
+      & dotnet test --collect:"XPlat Code Coverage" --logger trx Universe.SqlInsights.SqlServerStorage.Tests.dll
+
+      Say "Benchmark Seed"
+      Remove-Item -Path "$Env:SYSTEM_ARTIFACTSDIRECTORY/AddAction.log" -ErrorAction SilentlyContinue -Force
+      $ENV:TEST_UNDER_COVERAGE = "False"
+      & dotnet test --filter "Name~Test1_Seed" Universe.SqlInsights.SqlServerStorage.Tests.dll
--- scripts/CI-Steps/7-SqlJam-Tests.ps1
+++ scripts/CI-Steps/7-SqlJam-Tests.ps1
@@ -1,6 +1,6 @@
 . "$PSScriptRoot\Functions.ps1"
 cd C:\App\Goods\jam.tests
-$ENV:SENSORSAPP_STRESS_WORKINGSET_ROWS="10000"
-$ENV:SENSORSAPP_STRESS_DURATION="200"
+Set-Var "SENSORSAPP_STRESS_WORKINGSET_ROWS" "100000"
+Set-Var "SENSORSAPP_STRESS_DURATION" "2000"
 dotnet test Universe.SqlServerJam.Tests.dll
 Say "Success Complete"
