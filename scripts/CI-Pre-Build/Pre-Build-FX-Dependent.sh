7859839bc91122d3ef32b91458bc2f51e25a6ffa
devizer <devizer@users.github.io>
2026-02-12 14:50:37 +0200

    CI [skip ci]

---
+++ scripts/CI-Pre-Build/Pre-Build-FX-Dependent.sh
@@ -0,0 +1,26 @@
+          Say "Build fx-dependent [ErgoFab.DataAccess.IntegrationTests]"
+          pushd src/ErgoFab.DataAccess.IntegrationTests
+          time try-and-retry dotnet publish -c $configuration -o $SYSTEM_ARTIFACTSDIRECTORY/ergofab.tests -f net8.0
+          popd
+
+          Say "Build fx-dependent W3API"
+          pushd src/Universe.SqlInsights.W3Api
+          time try-and-retry dotnet publish -c $configuration -o $SYSTEM_ARTIFACTSDIRECTORY/w3api -f net6.0
+          popd
+
+          Say "Build fx-dependent [SqlServerStorage].Tests"
+          pushd src/Universe.SqlInsights.SqlServerStorage.Tests
+          time try-and-retry dotnet publish -c $configuration -o $SYSTEM_ARTIFACTSDIRECTORY/w3api.tests -f net6.0
+          popd
+
+          Say "Build fx-dependent [https://github.com/devizer/Universe.SqlServerJam].Tests"
+          git clone https://github.com/devizer/Universe.SqlServerJam ~/jam
+          pushd ~/jam/src/Universe.SqlServerJam.Tests
+          time try-and-retry dotnet publish -c $configuration -o $SYSTEM_ARTIFACTSDIRECTORY/jam.tests -f net6.0
+          popd
+
+          Say "Build fx-dependent [https://github.com/devizer/Universe.SqlServer.AdministrativeViews].Tests"
+          git clone https://github.com/devizer/Universe.SqlServer.AdministrativeViews ~/AdministrativeViews
+          pushd ~/AdministrativeViews/Universe.SqlServer.AdministrativeViews.Tests
+          time try-and-retry dotnet publish -c $configuration -o $SYSTEM_ARTIFACTSDIRECTORY/AdministrativeViews.tests -f net8.0
+          popd
---
+++ scripts/CI-Pre-Build/Pre-Build-w3app.sh
@@ -0,0 +1,6 @@
+         export NODE_VER=v16.20.2 SKIP_NPM_UPGRADE=True
+         time (script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node)
+         cd src/universe.sqlinsights.w3app
+         node --version
+         time try-and-retry yarn install
+         time yarn build
