52cde4e7337fd93bbd3f6eed7f4d677660a7354d
devizer <devizer@users.github.io>
2026-02-12 16:45:19 +0200

    CI [skip ci]

--- scripts/CI-Pre-Build/Copy-w3app-to-W3API-wwwroot.sh
+++ scripts/CI-Pre-Build/Copy-w3app-to-W3API-wwwroot.sh
@@ -1,5 +1,4 @@
         cd src/universe.sqlinsights.w3app/build
+        Say "Copying from $(pwd -P)"
         mkdir -p $SYSTEM_ARTIFACTSDIRECTORY/w3api/wwwroot
         cp -av * $SYSTEM_ARTIFACTSDIRECTORY/w3api/wwwroot/
-        Say "CONTENT FOR [$SYSTEM_ARTIFACTSDIRECTORY/w3api]"
-        ls -laR $SYSTEM_ARTIFACTSDIRECTORY/w3api
