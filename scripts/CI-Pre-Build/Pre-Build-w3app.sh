555095aa5b1976dd2a71864e19b1198115402bb6
devizer <devizer@users.github.io>
2026-02-12 15:04:13 +0200

    CI [skip ci]

--- scripts/CI-Pre-Build/Pre-Build-w3app.sh
+++ scripts/CI-Pre-Build/Pre-Build-w3app.sh
@@ -1,4 +1,4 @@
-         export NODE_VER=v16.20.2 SKIP_NPM_UPGRADE=True
+         export NODE_VER=v16.20.2 SKIP_NPM_UPGRADE=True CI=False
          time (script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node)
          cd src/universe.sqlinsights.w3app
          node --version
