35ce4e35d3064071891f87ca08776dc9ac599a9c
devizer <devizer@users.github.io>
2026-02-12 16:35:06 +0200

    CI [skip ci]

--- scripts/CI-Pre-Build/Pre-Build-w3app.sh
+++ scripts/CI-Pre-Build/Pre-Build-w3app.sh
@@ -1,5 +1,7 @@
          export NODE_VER=v16.20.2 SKIP_NPM_UPGRADE=True CI=False
-         time (script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node)
+         Say "BUILDING w3app usign NODE $NODE_VER"
+         # time (script=https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh; (wget -q -nv --no-check-certificate -O - $script 2>/dev/null || curl -ksSL $script) | bash -s node)
+         Run-Remote-Script https://raw.githubusercontent.com/devizer/glist/master/install-dotnet-and-nodejs.sh node
          cd src/universe.sqlinsights.w3app
          node --version
          time try-and-retry yarn install
