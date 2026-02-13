11a6fab0decc1b8db05770c4188645c535aa34ab
devizer <devizer@users.github.io>
2026-02-13 16:56:46 +0200

    CI [skip ci]

--- scripts/CI-Steps/999-Stop-Apps.ps1
+++ scripts/CI-Steps/999-Stop-Apps.ps1
@@ -4,7 +4,7 @@ Show-Dotnet-And-Chrome-Processes "BEFORE"
 
 foreach($name in "chrome.exe", "dotnet.exe", "dotnet-serve.exe") {
   Say "KILL $name"
-  & taskkill /f /t /im chrome.exe "$name"
+  & taskkill /f /t /im "$name"
 }
 
 Show-Dotnet-And-Chrome-Processes "AFTER"
