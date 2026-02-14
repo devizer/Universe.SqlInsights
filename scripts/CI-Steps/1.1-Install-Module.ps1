7842904f43efbd0c3ea17c39c7d25f8d58c9ccc1
devizer <devizer@users.github.io>
2026-02-15 00:52:40 +0200

    CI Matrix [skip ci]

--- scripts/CI-Steps/1.1-Install-Module.ps1
+++ scripts/CI-Steps/1.1-Install-Module.ps1
@@ -11,11 +11,10 @@ if ($PSVersionTable.PSEdition -ne "Core") {
   [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true};
 }
 
-$urlSource = 'https://devizer.github.io/SqlServer-Version-Management/Install-SqlServer-Version-Management.ps1'; 
+$urlSource = 'https://devizer.github.io/SqlServer-Version-Management/Install-SqlServer-Version-Management.ps1';
 foreach($attempt in 1..3) { try { iex ((New-Object System.Net.WebClient).DownloadString($urlSource)); Write-Host "Success: Install-SqlServer-Version-Management.ps1"; break; } catch {sleep 0.1;} }
 
 Say "Get-PS1-Repo-Downloads-Folder(): $(Get-PS1-Repo-Downloads-Folder)"
-
 
 
 Write-Artifact-Info "OS-NAME.TXT" "$(Get-OS-Name)"
