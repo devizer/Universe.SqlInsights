b285b84d78755227b7c504ccb69caaeae74efcdc
devizer <devizer@users.github.io>
2026-02-15 23:15:31 +0200

    CI Matrix, all steps [skip ci]

--- scripts/CI-Steps/3.1-Install-SQLCMD.ps1
+++ scripts/CI-Steps/3.1-Install-SQLCMD.ps1
@@ -5,8 +5,10 @@ $p="C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn"
 Add-Folder-To-User-Path $p
 Add-Folder-To-System-Path $p
 
+$instance_name = $ENV:SQL_INSTANCE_NAME
+if (-not $instance_name) { $instance_name = "(local)" }
 Say "SHOW SQL Collations"
-& sqlcmd -S "(local)" -h-1 -s"," -E -W -w 10000 -Q "SELECT Name, Description FROM fn_helpcollations() order by 1"
+& sqlcmd -S "$instance_name" -h-1 -s"," -E -W -w 10000 -Q "SELECT Name, Description FROM fn_helpcollations() order by 1"
 
 Say "SHOW DATABASE FILES"
-& sqlcmd -S "(local)" -h-1 -s"|" -E -W -w 10000 -Q "SELECT type_desc, name, physical_name FROM sys.database_files"
+& sqlcmd -S "$instance_name" -h-1 -s"|" -E -W -w 10000 -Q "SELECT type_desc, name, physical_name FROM sys.database_files"
