5b35f40dd8f8405f811adbb12ec09b68a976d5c4
devizer <devizer@users.github.io>
2026-02-16 04:26:10 +0200

    CI Matrix: Host Version '2022|2025' [skip ci]

--- .github/workflows/Set-Windows-Matrix.ps1
+++ .github/workflows/Set-Windows-Matrix.ps1
@@ -1,4 +1,4 @@
-param([string] $SqlSetSize = "MINI")
+param([string] $SqlSetSize = "MINI", [string] $HostVersion = "2022")
 Import-DevOps
 
 Enumerate-Plain-SQLServer-Downloads | % { [pscustomobject] $_ } | ft -autosize | out-string -width 222 | tee-object "$($ENV:SYSTEM_ARTIFACTSDIRECTORY)\Plain-SQLServer-Downloads.Table.txt"
@@ -8,6 +8,8 @@ foreach($meta in Enumerate-Plain-SQLServer-Downloads) {
   $sql = $meta.NormalizedKeywords
   # LocalDB x86 is not supported on 64-bit windows
   if ($sql -match "LocalDB" -and $sql -match "x86") { continue; }
+  # x86 v2012 and x86 v2014 are not supported on Windows 2025 Host
+  if ($HostVersion -eq "2025" -and ($sql -like '2012*' -or $sql -like '2014*') -and $sql -match 'x86') { continue; }
   # MINI Amount
   if ($SqlSetSize -eq "MINI") {
       $isMini = [bool] ($sql -match "Core" -and $sql -notmatch "Update")
@@ -18,10 +20,13 @@ foreach($meta in Enumerate-Plain-SQLServer-Downloads) {
       # LocalDB Only
       if ($sql -notmatch "LocalDB") { continue; }
   }
-  $run_on = '2022'
+  $run_on = "$HostVersion"
   $container_tag = $null
   if ($sql -like '2005*' -or $sql -like '2008*') { $container_tag = "2022" }
-  elseif ($sql -like '2012*' -or $sql -like '2014*') { $container_tag = "2016" } 
+  elseif ($sql -like '2012*' -or $sql -like '2014*') { 
+    # 2012 and 2014 run on 2022 Host, but need container on 2025
+    $container_tag = if ($HostVersion -eq "2025") { "2016" } Else { $null }
+  } 
   if ($sql -match 'LocalDB') { $container_tag=$null }
   $jobs += [pscustomobject] @{ SQL=$sql; HOST=$run_on; SQL_CONTAINER_SUFFIX=$container_tag }
 }
@@ -34,17 +39,17 @@ $matrix_object = @{ include = $jobs }
 $matrix_string_mini = $matrix_object | ConvertTo-Json -Depth 64 -Compress
 $matrix_string_formatted = $matrix_object | ConvertTo-Json -Depth 64
 
-Say "[Size $SqlSetSize] Github Windows Matrix Formatted-JSON"
+Say "[Size $SqlSetSize on $HostVersion] Github Windows Matrix Formatted-JSON"
 Write-Host $matrix_string_formatted
 
-Say "[Size $SqlSetSize] Github Windows Jobs Table"
+Say "[Size $SqlSetSize on $HostVersion] Github Windows Jobs Table"
 $jobs | ft -autosize | out-string -width 222 | tee-object "$($ENV:SYSTEM_ARTIFACTSDIRECTORY)\Windows-Jobs.Table.txt"
 
-Say "[Size $SqlSetSize] Github Windows Matrix Mini-JSON"
+Say "[Size $SqlSetSize on $HostVersion] Github Windows Matrix Mini-JSON"
 Write-Host $matrix_string_mini
 
 if ($env:GITHUB_OUTPUT) {
-  Say "[Size $SqlSetSize] Generating GITHUB_OUTPUT variable 'matrix'"
+  Say "[Size $SqlSetSize on $HostVersion] Generating GITHUB_OUTPUT variable 'matrix'"
   $utf8NoBom = New-Object System.Text.UTF8Encoding $false
   $outputLine = "matrix=$matrix_string_mini" + [System.Environment]::NewLine
   [System.IO.File]::AppendAllText($env:GITHUB_OUTPUT, $outputLine, $utf8NoBom)
