492031511de245af68ac843428f5634e338b37aa
devizer <devizer@users.github.io>
2026-02-17 19:56:09 +0200

    Linux Tests [skip ci]

--- scripts/CI-Steps/9-Run-w3app.ps1
+++ scripts/CI-Steps/9-Run-w3app.ps1
@@ -1,6 +1,6 @@
 . "$PSScriptRoot\Functions.ps1"
 
-cd C:\App\Goods\w3api\wwwroot
+cd Goods\w3api\wwwroot
 
 Say "index.html as file"
 Get-Content "index.html"
@@ -12,14 +12,15 @@ Add-Folder-To-User-Path "$($ENV:USERPROFILE)\.dotnet\tools"
 $ENV:PATH="$($ENV:USERPROFILE)\.dotnet\tools;$($ENV:PATH)"
 
 Say "About dotnet serve"
-dotnet serve --version 2>$null
+& dotnet serve --version 2>$null
 
 Say "LAUNCHING w3app on port 6060 (folder is '$(Get-Location)')"
 # cmd /c "start dotnet serve -p 6060"
 # Start-Process "dotnet" -ArgumentList "serve -p 6060".Split(" ") -NoNewWindow
 # & { dotnet Universe.SqlInsights.W3Api.dll } &
 # & { dotnet serve -p 6060 } &
-cd C:\App\Goods\w3api\wwwroot
+# ALREADY AT cd C:\App\Goods\w3api\wwwroot
+
 Smart-Start-Process "dotnet" "serve -p 6060"
 sleep 2
 
@@ -34,8 +35,8 @@ Show-Chrome-Processes
 
 
 Say "Validate http connection to http://localhost:6060"
-& curl.exe -I http://localhost:6060
+& (Get-Command curl -CommandType Application | Select-Object -First 1).Source -I http://localhost:6060
 Say "curl http://localhost:6060"
-& curl.exe http://localhost:6060
+& (Get-Command curl -CommandType Application | Select-Object -First 1).Source http://localhost:6060
 
 Say "Frontend launch completed"
