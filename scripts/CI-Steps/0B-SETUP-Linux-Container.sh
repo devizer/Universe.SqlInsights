3f26d5af29e283dfec7d485aa51ca9c0ae505056
devizer <devizer@users.github.io>
2026-02-18 19:16:49 +0200

    CI Matrix: libssl on linux, LINUX_MSSQL_PID="Developer|Express,", Jam Tests on Linux, Renamed Jobs and files, Chrome on linux and Windows, Find-Chrome-Program-List on Windows And Linux [skip ci]

--- scripts/CI-Steps/0B-SETUP-Linux-Container.sh
+++ scripts/CI-Steps/0B-SETUP-Linux-Container.sh
@@ -1,5 +1,19 @@
       set -eu; set -o pipefail;
       Say "CPU: $(Get-CpuName)"
+
+
+  Say "Existing Lib SSL binaries"
+  ldconfig -p | { grep "libssl\|libcrypto" || true; }
+  Say "Install Lib SSL 1.1 if required"
+  export INSTALL_DIR=/usr/local/libssl-1.1
+  mkdir -p $INSTALL_DIR
+  printf "\n$INSTALL_DIR\n" | sudo tee -a /etc/ld.so.conf >/dev/null || true
+  Run-Remote-Script https://raw.githubusercontent.com/devizer/glist/master/install-libssl-1.1.sh
+  ldconfig || true
+  ldconfig -p | { grep "libssl\|libcrypto" || true; }
+  export INSTALL_DIR=
+
+      Say "Starting LINUX Container"
       v="";
       test -n "${RAM_DISK:-}" && v="-v ${RAM_DISK}:${RAM_DISK} -e MSSQL_DATA_DIR=${RAM_DISK}"
       echo "SQL Server Container Volume Options: [$v]"
