b03fa4c20745818b2578f436f6f456c7dc150aa2
devizer <devizer@users.github.io>
2026-02-19 06:00:56 +0200

    CI Matrix: Microsoft provider on Linux, System On Windows, libssl on linux, LINUX_MSSQL_PID="Developer|Express,", Jam Tests on Linux, Renamed Jobs and files, Chrome on linux and Windows, Find-Chrome-Program-List on Windows And Linux [skip ci]

--- scripts/CI-Steps/3-Install-SQL-Server.ps1
+++ scripts/CI-Steps/3-Install-SQL-Server.ps1
@@ -35,7 +35,7 @@ if ("$ENV:SQL" -match "LocalDB") {
 
 echo "Query SQL Server '$ENV:SQL_INSTANCE_NAME' Medium Version"
 try { 
-  $sql_ver = Query-SqlServer-Version -Title "Instance $ENV:SQL_INSTANCE_NAME" -Instance "$ENV:SQL_INSTANCE_NAME"
+  $sql_ver = Query-SqlServer-Version -Title "Instance $ENV:SQL_INSTANCE_NAME" -ConnectionString "$ENV:SQLINSIGHTS_CONNECTION_STRING"
   if ($sql_ver) { 
     Write-Line -TextGreen "Query-SqlServer-Version for Medium Version SUCCESS: $sql_ver"
     Write-Artifact-Info "SQL-SERVER-MEDIUM-VERSION.TXT" "$sql_ver" 
@@ -45,7 +45,7 @@ catch {}
 
 echo "Query SQL Server '$ENV:SQL_INSTANCE_NAME' Title"
 try { 
-  $sql_ver = Query-SqlServer-Version -Title "Instance $ENV:SQL_INSTANCE_NAME" -Instance "$ENV:SQL_INSTANCE_NAME" -Kind "Title"
+  $sql_ver = Query-SqlServer-Version -Title "Instance $ENV:SQL_INSTANCE_NAME" -ConnectionString "$ENV:SQLINSIGHTS_CONNECTION_STRING" -Kind "Title"
   if ($sql_ver) {
     Write-Line -TextGreen "Query-SqlServer-Version for Title SUCCESS: $sql_ver"
     Write-Artifact-Info "SQL-SERVER-TITLE.TXT" "$sql_ver"
