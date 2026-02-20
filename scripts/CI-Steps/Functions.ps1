8f362f1c664902c97bed1bdbb7f3f777077f2f33
devizer <devizer@users.github.io>
2026-02-20 19:13:11 +0200

    CI Matrix: Explicit Throw after tests,  Microsoft provider on Linux, System On Windows, libssl on linux, LINUX_MSSQL_PID="Developer|Express,", Jam Tests on Linux, Renamed Jobs and files, Chrome on linux and Windows, Find-Chrome-Program-List on Windows And Linux [skip ci]

--- scripts/CI-Steps/Functions.ps1
+++ scripts/CI-Steps/Functions.ps1
@@ -378,8 +378,9 @@ Set-Var "SQLINSIGHTS_REPORT_FULLNAME" "$SQLINSIGHTS_REPORT_FULLNAME"
   $ERGOFAB_TESTS_DATA_FOLDER = If ((Get-OS-Platform) -eq "Windows") { "$($root_drive)\ergo-fab-tests" } Else { "/mnt/ergo-fab-tests" }
   Set-Var "ERGOFAB_TESTS_DATA_FOLDER" "$ERGOFAB_TESTS_DATA_FOLDER"
   Set-Var "ERGOFAB_TESTS_MASTER_CONNECTIONSTRING" "TrustServerCertificate=True;Data Source=$sql_instance_name;$sql_security_parameters;Encrypt=False;"
-  Set-Var "ERGOFAB_TESTS_HISTORY_CONNECTIONSTRING" "Server=$sql_instance_name;Encrypt=False;Initial Catalog=SqlInsights Local Warehouse;$sql_security_parameters;"
-  Set-Var "ERGOFAB_TESTS_REPORT_FULLNAME" "$($ENV:SYSTEM_ARTIFACTSDIRECTORY)\ErgFab Tests Report.txt"
+  Set-Var "ERGOFAB_TESTS_HISTORY_CONNECTIONSTRING" "Server=$sql_instance_name;Encrypt=False;Initial Catalog=SqlInsights Local Warehouse;Pooling = true;$sql_security_parameters;"
+  $ERGOFAB_TESTS_REPORT_FULLNAME = Combine-Path "$($ENV:SYSTEM_ARTIFACTSDIRECTORY)" "ErgFab Tests Report.txt"
+  Set-Var "ERGOFAB_TESTS_REPORT_FULLNAME" "$ERGOFAB_TESTS_REPORT_FULLNAME"
   $ERGOFAB_SQL_PROVIDER = if ((Get-OS-Platform) -ne "Windows") { "Microsoft" } Else { "System" }
   Set-Var "ERGOFAB_SQL_PROVIDER" "Microsoft"
 
