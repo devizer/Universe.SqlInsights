7c6529f92a183f20cf87139df355deb2afbb4145
devizer <devizer@users.github.io>
2026-03-07 01:32:00 +0200

    Fix Run-CI-Script; Brush up Build-NuGET-Array.ps1, Fixed Shared Backup Locking for Universe.NUnitPipeline.SqlServerDatabaseFactory/SeededDatabaseFactory.cs [skip ci]

--- scripts/CI-Steps/4-Publish-Sql-Server-Setup-Logs.ps1
+++ scripts/CI-Steps/4-Publish-Sql-Server-Setup-Logs.ps1
@@ -1,3 +1,6 @@
+Write-Line -TextCyan "ENV:SQL = [$($ENV:SQL)]"
 $key="$($ENV:SQL)".Split(":")[0]
-Publish-SQLServer-SetupLogs "C:\App\SQL Setup Logs\$key"
+$target_logs_folder="C:\App\SQL Setup Logs\$key"
+Write-Line -TextCyan "target_logs_folder = [$target_logs_folder]"
+Publish-SQLServer-SetupLogs $target_logs_folder
 
