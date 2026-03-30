f652411583489b8f53b14442ad043bf615fae376
devizer <devizer@users.github.io>
2026-03-30 21:17:10 +0300

    Actions pipeline: Added self hosted sse3 only build agent [skip ci]

--- scripts/CI-Steps/8-Install-Chrome-on-Linux.sh
+++ scripts/CI-Steps/8-Install-Chrome-on-Linux.sh
@@ -1,8 +1,9 @@
       set -eu; set -o pipefail
       try-and-retry sudo apt-get install xdg-utils -y -qq
+      try-and-retry sudo apt-get install fonts-liberation -y -qq
+      
       try-and-retry curl -kfSL -o /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-      sudo dpkg -i /tmp/chrome.deb
-      sudo apt-get install -f -y
+      sudo dpkg -i /tmp/chrome.deb || sudo apt-get install -f
       printf "/usr/bin/env bash\n port=9234; google-chrome --headless --disable-gpu --no-first-run --no-sandbox \"\$@\"\n" | sudo tee /usr/local/bin/www-browser
       sudo chmod +x /usr/local/bin/www-browser
       for c in google-chrome www-browser; do
