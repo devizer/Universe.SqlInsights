25d59d1d6fb2e80fb5f7fc44a3b6c3cefab18e5f
devizer <devizer@users.github.io>
2026-04-07 18:24:14 +0300

    Github Actions: Brush up workflow [skip ci]

--- scripts/CI-Steps/Broard-Cast-Demo.ps1
+++ scripts/CI-Steps/Broard-Cast-Demo.ps1
@@ -1,16 +1,16 @@
 Function BroadCast-Variables() {
     try {
-        # Сигнатура Win32 API
+        # Win32 API
         $signature = '[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
                       public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, UIntPtr wParam, string lParam, uint fuFlags, uint uTimeout, out UIntPtr lpdwResult);'
 
-        # Добавляем тип (с проверкой, чтобы избежать ошибок при повторном вызове)
+        # Check up if type already compiled
         if (-not ([System.Management.Automation.PSTypeName]"Win32.NativeMethods").Type) {
             Add-Type -MemberDefinition $signature -Name "NativeMethods" -Namespace "Win32"
         }
 
         $result = [UIntPtr]::Zero
-        # Используем [ref] вместо out
+        # out --> [ref]
         $ret = [Win32.NativeMethods]::SendMessageTimeout([IntPtr]0xffff, 0x001A, [UIntPtr]::Zero, "Environment", 0x02, 1000, [ref]$result)
         
         Write-Line -TextGreen "Bradcast variables success, SendMessageTimeout() --> $ret, [ref] result = $result"
--- src/ErgoFab.DataAccess.IntegrationTests/Test-via-NuGET.sh
+++ src/ErgoFab.DataAccess.IntegrationTests/Test-via-NuGET.sh
@@ -3,7 +3,8 @@ function Get-Nuget-package-Latest-Version() {
   local folder="console$RANDOM"
   dotnet new console -o "$folder" >/dev/null; 
   cd $folder
-  dotnet add package "$1" >/dev/null
+  Say "Query package [$1] version" >&2
+  time dotnet add package "$1" >/dev/null
   cat *.csproj | grep PackageReference | grep -Eo "([0-9]{1,}\.)+[0-9]{1,}" | tail -1
   cd ..
   rm -rf $folder >/dev/null 2>&1
@@ -23,8 +24,14 @@ echo "Universe.SqlInsights.NUnit   latest release = [$(Get-Nuget-package-Latest-
 ver="3.14.0.$build_number"
 sed -i '/Universe.NUnitPipeline.SqlServerDatabaseFactory.csproj/d' "$prj"
 sed -i '/Universe.SqlInsights.NUnit.csproj/d' "$prj"
+
+Say "Add Package [Universe.NUnitPipeline.SqlServerDatabaseFactory]"
 dotnet add "$prj" package Universe.NUnitPipeline.SqlServerDatabaseFactory -v "$ver" --no-restore
+Say "Add Package [Universe.SqlInsights.NUnit]"
 dotnet add "$prj" package Universe.SqlInsights.NUnit -v "$ver" --no-restore
 echo "FINAL [$prj]"
 cat "$prj"
+time dotnet build -c Release -f net8.0
+Say "TESTING ...."
 dotnet test -c Release -f net8.0
+Say "Finish tests"
